ef8d80646ad12183feb3ec33cce313b33a3c5557
Muizz Lateef <lateefmuizz@gmail.com>
2026-01-13 17:00:13 +0000

    done

---
+++ models/metricflow_time_spine.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized='table',
+    )
+}}
+
+with days as (
+    {{
+        dbt.date_spine(
+            'day',
+            "cast('2020-01-01' as date)",
+            "cast('2030-12-31' as date)"
+        )
+    }}
+),
+
+final as (
+    select cast(date_day as date) as date_day
+    from days
+)
+
+select * from final
